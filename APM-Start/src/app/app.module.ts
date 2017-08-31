3e0c940bbae4ff955011ba737d9c6aaaa3de49f4
Tom Stagg <tomstagg@gmail.com>
2017-08-31 23:18:37 +0100

    router setup

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 
+import { ProductListComponent } from './products/product-list.component';
 import { AppComponent } from './app.component';
+import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
+import { StarComponent } from "./shared/star.component";
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from "./home/welcome.component";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent},
+      { path: 'products/:id', component: ProductDetailComponent},
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
